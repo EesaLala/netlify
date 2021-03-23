468f49b32d2cdb3a71b78c18e41c3aa37b67a1b0
Eesa <eesa.lala@hotmail.com>
2021-03-23 20:37:09 +0000

    test js3

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-const menuToggle = document.querySelector('.toggle');
-const showcase = document.querySelector('.showcase');
+// const menuToggle = document.querySelector('.toggle');
+// const showcase = document.querySelector('.showcase');
 
-menuToggle.addEventListener('click', () => {
-  menuToggle.classList.toggle('active');
-  showcase.classList.toggle('active');
-})
+// menuToggle.addEventListener('click', () => {
+//   menuToggle.classList.toggle('active');
+//   showcase.classList.toggle('active');
+// })
 
-const toggle = document.getElementById('toggle');
-const close = document.getElementById('close');
-const open = document.getElementById('open');
-const modal = document.getElementById('modal');
+// const toggle = document.getElementById('toggle');
+// const close = document.getElementById('close');
+// const open = document.getElementById('open');
+// const modal = document.getElementById('modal');
 
 
 
-open.addEventListener('click',() => modal.classList.add('show-modal'));
+// open.addEventListener('click',() => modal.classList.add('show-modal'));
 
-close.addEventListener('click',() => modal.classList.remove('show-modal'));
+// close.addEventListener('click',() => modal.classList.remove('show-modal'));
 
-//hide modal on outside click
+// //hide modal on outside click
 
-window.addEventListener('click', e => e.target == modal ? modal.classList.remove('show-modal') : false);
+// window.addEventListener('click', e => e.target == modal ? modal.classList.remove('show-modal') : false);
